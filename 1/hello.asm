8979129bfeaa993292521e0511bf6ceabd4b2aff
cmsxbc <i@cmsis.me>
2020-11-25 21:19:19 +0800

    smaller header

--- 1/hello.asm
+++ 1/hello.asm
@@ -3,15 +3,16 @@ BITS 64
 global    _start
 
 ehdr:           ; Elf64_Ehdr
-  db 0x7f, "ELF", 2, 1, 1, 0 ; e_ident
-  times 8 db 0
-  ; dd 0x67452301
-  ; dd 0xefcdab89
-; _start:
+  db 0x7f, "ELF", 2, 1, 1
+  ; db 0 ; e_ident
+  ; times 8 db 0
 
-;   inc       al
-;   shl       rax, 32
-;   jmp     prepare_stack
+exit:
+     syscall
+     mov       al, 60
+     dec       dil
+     jmp       exit
+
   dw  2         ; e_type
   dw  0x3e      ; e_machine
   dd  1         ; e_version
@@ -21,21 +22,21 @@ ehdr:           ; Elf64_Ehdr
   dd  0         ; e_flags
   dw  ehdrsize  ; e_ehsize
   dw  phdrsize  ; e_phentsize
-  dw  1         ; e_phnum
-  dw  0         ; e_shentsize
-  dw  0         ; e_shnum
-  dw  0         ; e_shstrndx
-; phdr:
-;   dw  1         ; e_phnum
-;   dw  0         ; e_shentsize
-;   dw  7         ; e_shnum
-;   dw  0         ; e_shstrndx
-ehdrsize  equ  $ - ehdr
+  ; dw  1         ; e_phnum
+  ; dw  0         ; e_shentsize
+  ; dw  0         ; e_shnum
+  ; dw  0         ; e_shstrndx
 phdr:
   dw  1         ; e_phnum
   dw  0         ; e_shentsize
   dw  7         ; e_shnum
   dw  0         ; e_shstrndx
+ehdrsize  equ  $ - ehdr
+; phdr:
+;   dw  1         ; e_phnum
+;   dw  0         ; e_shentsize
+;   dw  7         ; e_shnum
+;   dw  0         ; e_shstrndx
   dq  0         ; p_offset
   dq  $$        ; p_vaddr
   dq  $$        ; p_paddr
@@ -199,11 +200,7 @@ output:
           mov       dl, 33  ; outputcount
           mov       al, 1
           mov       dil, 1    ; fd of stdout
-          syscall
-exit:
-          mov       al, 60
-          dec       dil
-          syscall
+          jmp       exit
 
 
 rotates   db 7, 12, 17, 22
