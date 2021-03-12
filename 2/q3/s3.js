f3e6977e44fcaae7aa6cc5484a0e7b2e507ecf58
cmsxbc <i@cmsis.me>
2021-03-13 01:32:18 +0800

    s3 done

--- 2/q3/s3.js
+++ 2/q3/s3.js
@@ -9,3 +9,40 @@ window.A593C8B8 = async (_) => (($, _, __, ___, ____) => {
     };
     return new Promise(__ => _["a"]["forEach"](___ => $["setTimeout"](____ => ______(___, _____, __), ___)))
 })(window, _, +[], +[], +[])
+
+
+window.A593C8B8 = async (param) => (($, param, count, ___, ____) => {
+    let generator = function*() {
+        while (true) {
+            yield [
+                (a, b) => a + b,
+                (a, b) => a - b,
+                (a, b) => a * b
+            ][++count % 3]["bind"](0, ___, ____)
+        }
+    }();
+    let calc = function(val, gen, resolve) {
+        ____ = val;
+        let b = ___;
+        ___ = gen["next"]()["value"]();
+        console.log(b, ____, ___);
+        count == param["a"]["length"] && resolve(-___)
+    };
+    return new Promise(resolve => param["a"]["forEach"](val => $["setTimeout"](____ => calc(val, generator, resolve), val)))
+})(window, param, 0, 0, 0)
+
+
+window.A593C8B8 = async function (param) {
+    let values = param['a'].sort((a, b) => (a > b));
+    let ret = 0;
+    let funcs = [
+        (a, b) => a + b,
+        (a, b) => a - b,
+        (a, b) => a * b
+    ];
+    for (var i = 0; i < values.length; i++) {
+        let b = ret;
+        ret = funcs[(i+1) % 3](ret, values[i]);
+    }
+    return -ret;
+}
